059c3679247f91dd18d759fa65ef01cc8a449fd7
卜祥录 <buxl@pv25.com>
2018-07-24 17:02:53 +0800

    vender

---
+++ modules/forum/Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2018/7/23
+ * Time: 16:11
+ */
+
+namespace app\modules\forum;
+
+
+class Moudle
+{
+
+}
---
+++ modules/forum/controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2018/7/23
+ * Time: 16:15
+ */
+
+namespace app\modules\forum\controllers;
+
+
+class PostController
+{
+
+}
